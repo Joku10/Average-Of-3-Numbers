ebca77face8df5c15f147146f42784b0e3838747
Lijo Jolly <lijojolly10@gmail.com>
2024-03-09 23:06:53 +0530

    Add files via upload

---
+++ Average of 3 Numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+ 
+int main() { 
+    float num1, num2, num3, average; 
+     
+    printf("Enter the first number: "); 
+    scanf("%f", &num1); 
+     
+    printf("Enter the second number: "); 
+    scanf("%f", &num2); 
+
+    printf("Enter the third number");
+    scanf("%f", &num3);
+     
+    average = (num1 + num2 + num3) / 2; 
+     
+    printf("The average is: %f", average); 
+}
